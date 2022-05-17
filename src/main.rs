79c4b6475687bee53066ff220dc0bdc087c8657a
Brandon Davies <daviesbl9@cardiff.ac.uk>
2022-05-17 22:59:25 +0100

    Rust user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::io;
+
 fn main() {
     println!("Hello, world!");
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    println!("{}", input);
 }
