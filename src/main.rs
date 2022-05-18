aa32f1c149b0e56f828f435994a3d45a7996841d
Brandon Davies <daviesbl9@cardiff.ac.uk>
2022-05-18 20:43:29 +0100

    Complete guessing game with colour text

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,35 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+use colored::Colorize;
 
 fn main() {
-    println!("Hello, world!");
-    let mut input = String::new();
+    println!("Guess a number!");
+    // Generate random number
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    
+    loop {
 
-    io::stdin().read_line(&mut input).expect("Failed to read line");
-    println!("{}", input);
+        // Creates empty string and Takes a guess
+        println!("Please input a number.");
+        let mut guess: String = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+        // Change to int32
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{}", "Too small!,  Guess again!".red()),
+            Ordering::Greater => println!("{}", "Too big!, Guess again!".red()),
+            Ordering::Equal => {
+                println!("{}", "You win!, Congratulations!".green());
+                break;
+            },
+        }  
+    }
 }
